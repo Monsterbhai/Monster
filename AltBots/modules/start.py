dbf02295e00c16d72354427cf7208835b0d46703
Monster <164752224+Monsterbhai@users.noreply.github.com>
2024-06-23 16:32:43 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -8,12 +8,9 @@ START_BUTTON = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
     ],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/O_P_Hacker"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/O_P_Hacker")
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/monstarlove"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/monstercopyright1")
     ],
-    [
-        Button.url("• ʀᴇᴘᴏ •", "https://te.legra.ph/file/d106519f324f3309b23eb.jpg")
-    ]
 ]
 
 
@@ -34,13 +31,13 @@ async def start(event):
         bot_name = AltBot.first_name
         bot_id = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝐇𝐀𝐂𝐊𝐄𝐑](https://t.me/legend_of_all_groups)**\n\n"
-        TEXT += f"» **𝐇𝐀𝐂𝐊𝐄𝐑 𝐁𝐎𝐓 ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝐌𝐎𝐍𝐒𝐓𝐄𝐑](https://t.me/patel1154)**\n\n"
+        TEXT += f"» **𝐌𝐎𝐍𝐒𝐓𝐄𝐑 𝐁𝐎𝐓𝐒 ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                     event.chat_id,
-                    "https://te.legra.ph/file/d106519f324f3309b23eb.jpg",
+                    "https://telegra.ph/file/67433e93e05897c227609.jpg",
                     caption=TEXT, 
                     buttons=START_BUTTON
                 )
